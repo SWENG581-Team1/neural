fd18ea97cc3ddf4565c0235de1e28eeb67617409
Andy Nortrup <andrew.nortrup@gmail.com>
2016-04-10 21:55:09 -0700

    Modified the test to check the point_value method of gin_card

--- test_gin_deck_psu.py
+++ test_gin_deck_psu.py
@@ -3,8 +3,15 @@ from gindeck import *
 
 def test_deal_a_card():
     mockDeck = MagicMock()
-    mockDeck.deal_a_card.return_value = Card('c', 1)
+    mockDeck.deal_a_card.return_value = Card(2, 'c')
+    mockDeck.deal_a_card.return_value = Card(11, 'd')
 
+    #Test that we get the top card back from the mock
     gd = GinDeck(mockDeck)
     result = gd.deal_a_card()
-    assertEqual(result, GinCard('c', 1))
+    assertEqual(result, GinCard(2, 'c'))
+    assertEqual(result.point_value, 2)
+
+    #Test that the Gin Card returns the proper value
+    result = gd.deal_a_card()
+    assertEqual(result.point_value, 10)
